4d7919c89963b438e753d65339abdb8d444a000c
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-11-25 00:30:25 +0200

    Created layouts with flexbox
    About flexbox:
    	- elements are positioned inside of containers
    	- positioning is controlled via style settings applied to the
    	  element container

--- App.js
+++ App.js
@@ -1,26 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>Hello</Text>
-      <Button title="Click me" onPress={() => alert('Button clicked')} />
+    <View style={styles.appContainer}>
+      <View style={styles.inptContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Course Goal" />
+        <Button title="ADD goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
   },
-  dummyText: {
-    mergin: 16,
+  inptContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
     borderWidth: 1,
-    borderColor: 'magenta',
-    padding: 16,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
   },
 });
