a7791b7911d8bdb853449dcc69d0d88ecf4c476b
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2024-11-25 23:02:33 +0200

    Add dynamic goal list rendering & state managament
    - Implemented state management for goals using `useState`
    - Rendered the goal list dynamically using `map` method
    - Added unique key handling for list items

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [enteredGoalText, setEnteredText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoalText]);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your Course Goal" />
-        <Button title="ADD goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Course Goal"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="ADD goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals...</Text>
+        {courseGoals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
       </View>
     </View>
   );
